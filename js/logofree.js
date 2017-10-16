3459cd2390a5b0fb7da1e0989be9ad9d58745cc7
jackLei <threetonws@163.com>
2017-10-16 17:10:24 +0800

    [javascript] Add: trigger step 2 feature.

--- js/logofree.js
+++ js/logofree.js
@@ -19,11 +19,41 @@ $.loadForm = function(){
 						slogan: {text:$('[name=slogan]').val()},
 						trade: $('[name=trade]').val()
 					}
+					$.loadMaterial();
 				}
 			})
 		});
 		return false;
 	});
+}
+
+// 第二步：展示模板
+$.loadMaterial = function(){
+    var _DOM = '.js-filter-logo';
+    $.progress(2);
+
+    $.post(listUrl, $.svgParams, function(json){
+        // 生成模板列表
+        $('.selectLogoList', _DOM).empty();
+        if (json.length) {
+            $(json).each(function(){
+                $('.selectLogoList', _DOM).append('<li data-id="'+this.id+'"><i class="icon-select"></i><img src="'+ BaseUrl + '/openapi/logo/materialPreview?material_id='+this.id+'&name='+$.svgParams.name.text+'&slogan='+$.svgParams.slogan.text +'" alt=""></li>')
+            });
+        } else {
+            $('.selectLogoList', _DOM).html('<div class="notfound">暂无相关图形</div>');
+        }
+        // 点击列表，显示VI图
+        $('li', _DOM).on('click', function(){
+            var id = $(this).data('id');
+            $(this).addClass('active').siblings('li').removeClass('active');
+
+            $.svgParams.material_id = id;
+            var url = BaseUrl + '/openapi/logo/materialPreview?material_id='+id+'&name='+$.svgParams.name.text+'&slogan='+$.svgParams.slogan.text;
+            $.post(logoTplUrl, {logourl: url}, function(html){
+                $('.logoApplicationList').html(html.tpl);
+            });
+        });
+    },'json');
 }
 
 $(function(){
